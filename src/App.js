d837f0ebc373e32dc612a2119685e345c87c53ca
zodac <arouge110@msn.com>
2023-08-25 20:10:58 +1200

    Moving Hardware table into own class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Table from "./components/Table";
 import testTableData from "./test/tableData.json";
-import testHardwares from "./test/hardwares.json";
+import HardwareTable from "./components/HardwareTable";
 
 const App = () => {
 
@@ -10,16 +10,6 @@ const App = () => {
         { label: "Gender", accessor: "gender", sortable: true, type: "string" },
         { label: "Age", accessor: "age", sortable: true, type: "string" },
         { label: "Start date", accessor: "start_date", sortable: true, type: "string" },
-    ];
-
-    const hardwareColumns = [
-        { label: "ID", accessor: "id", sortable: true, sortByOrder: "asc", type: "int" },
-        { label: "Hardware Name", accessor: "hardwareName", sortable: false, type: "string" },
-        { label: "Display Name", accessor: "displayName", sortable: true, type: "string" },
-        { label: "Hardware Make", accessor: "hardwareMake", sortable: true, type: "string" },
-        { label: "Hardware Type", accessor: "hardwareType", sortable: true, type: "string" },
-        { label: "Multiplier", accessor: "multiplier", sortable: true, type: "double" },
-        { label: "Average PPD", accessor: "averagePpd", sortable: true, type: "int" },
     ];
 
     return (
@@ -33,11 +23,7 @@ const App = () => {
             <br/>
             <br/>
             <br/>
-            <Table
-                caption="Hardware on the system"
-                data={testHardwares}
-                columns={hardwareColumns}
-            />
+            <HardwareTable/>
         </div>
     );
 };
